b67c1a58d465166217cd4ebdcacd91adb530b419
Vardhman Shaha <vardhmanshaha@gmail.com>
2024-10-29 17:33:46 +0000

    Portfolio version updated df

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
