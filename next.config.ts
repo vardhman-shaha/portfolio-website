37e622059d1b567d6f4e2c5ecca663da6191df67
Vardhman Shaha <vardhmanshaha@gmail.com>
2024-10-30 05:23:36 +0000

    Portfolio version updated df

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,11 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
-  /* other config options here */
+  basePath: '/portfolio-website', // Adjust this if your repo name is different
 };
 
 export default nextConfig;
